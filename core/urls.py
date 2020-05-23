6c159b1c870125cb45a4644b5488acbe22c3b037
Uglaos <alenuglik@gmail.com>
2020-05-23 11:41:15 +0200

    [ADD] Adding/removing items from cart

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, checkout, ItemDetailView
+from .views import HomeView, checkout, ItemDetailView, add_to_cart, remove_from_cart
 
 app_name = 'core'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
 ]
