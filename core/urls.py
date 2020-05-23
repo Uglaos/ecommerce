5059321ed6c45facd617757ccc8bf0ffa7b2376f
Uglaos <alenuglik@gmail.com>
2020-05-23 10:06:56 +0200

    [ADD] Class based views, category and label choices

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import home, checkout, products
+from .views import HomeView, checkout, ItemDetailView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
-    path('products/', products, name='products'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
 ]
